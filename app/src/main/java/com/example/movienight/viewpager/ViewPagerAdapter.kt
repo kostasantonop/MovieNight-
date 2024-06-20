8141281dd8f582ac7647ed4e8009bf673a0335ab
Konstantinos Antonopoulos <kostas_antonop@hotmail.com>
2024-06-20 11:54:55 +0300

    fully developed functionality (without API) of tab2

--- app/src/main/java/com/example/movienight/viewpager/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/movienight/viewpager/ViewPagerAdapter.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.movienight.viewpager.tab1recyclerview.Tab1RecyclerViewFragment
+import com.example.movienight.viewpager.tab2recyclerview.Tab2RecyclerViewFragment
 
 class ViewPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
 
@@ -14,8 +15,8 @@ class ViewPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activ
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> Tab1RecyclerViewFragment()
-            1 -> Tab3Fragment()
-            2 -> Tab2Fragment()
+            1 -> Tab2RecyclerViewFragment()
+            2 -> Tab3Fragment()
             else -> Tab1RecyclerViewFragment()
         }
     }
