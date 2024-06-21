69eeb844f1f8f06c0e4729ede8b56184974ed47b
Tsoutsis Spyros <spirosposadas@gmail.com>
2024-06-22 01:30:51 +0300

    movie data class

--- app/src/main/java/com/example/movienight/MainActivity.kt
+++ app/src/main/java/com/example/movienight/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.movienight
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.movienight.databinding.ActivityMainBinding
 import com.example.movienight.viewpager.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayout
+import com.tapadoo.alerter.Alerter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewPagerAdapter: ViewPagerAdapter
+
+
+    private lateinit var connectivityManager: ConnectivityManager
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,5 +51,83 @@ class MainActivity : AppCompatActivity() {
                 binding.tabLayout.getTabAt(position)?.select()
             }
         })
+
+        //connectivity manager
+        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        networkCallback = object : ConnectivityManager.NetworkCallback() {
+            //network available case
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                notifyNetworkChange(true)
+                this.let {
+                    Alerter.create(this@MainActivity)
+                        .setTitle("NetWork available")
+                        .setText("There are available networks to connect.")
+                        .setDuration(10000)
+                        .setBackgroundColorRes(R.color.green)
+                        .show()
+                }
+            }
+
+            //network lost case
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                notifyNetworkChange(false)
+                this.let {
+                    Alerter.create(this@MainActivity)
+                        .setTitle("NetWork lost...")
+                        .setText("It seems your connection is lost. Please check your connection or connect to another network.")
+                        .setDuration(10000)
+                        .setBackgroundColorRes(R.color.red)
+                        .show()
+                }
+            }
+
+            //network change case
+            override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                super.onCapabilitiesChanged(network, networkCapabilities)
+
+                val hasInternet = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                notifyNetworkChange(hasInternet)
+                this.let {
+                    Alerter.create(this@MainActivity)
+                        .setTitle("Network Connected.")
+                        .setText("Please make sure you are on a safe network.")
+                        .setDuration(10000)
+                        .setBackgroundColorRes(R.color.green)
+                        .show()
+
+                }
+            }
+        }
+
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun notifyNetworkChange(isConnected: Boolean) {
+        // Notify all fragments or any component interested in network changes
+        supportFragmentManager.fragments.forEach { fragment ->
+            if (fragment is NetworkChangeListener) {
+                fragment.onNetworkChanged(isConnected)
+            }
+        }
+    }
+
+    interface NetworkChangeListener { //αυτο θα γινεται implement απο τα fragment ωστε να μπορουν να καλουν την onNetworkChanged
+
+        //αυτη θα καλειται απο το καθε fragment ωστε να γινεται ο ελεγχος συνδεσης
+        fun onNetworkChanged(isConnected: Boolean)
+    }
+
 }
---
+++ app/src/main/java/com/example/movienight/Movie.kt
@@ -0,0 +1,8 @@
+package com.example.movienight
+
+data class Movie(
+    var title : String,
+    var rate : Double,
+    var info : String,
+    var favourite : Boolean
+)
